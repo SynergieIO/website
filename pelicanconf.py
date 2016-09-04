87cee8c9936ea9333c9cc45abf2a7cd6843eafff
Jean-François Nguyen <jf@lse.epita.fr>
2016-09-04 17:11:34 +0200

    add pelicanconf.py

---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Synergie'
+SITENAME = 'Synergie'
+SITEURL = ''
+DESCRIPTION = 'Agir en faveur de la mixité dans les métiers de l\'informatique'
+
+PATH = 'content'
+THEME = 'themes/synergie-monospace'
+
+LOCALE = ("fr_FR")
+TIMEZONE = 'Europe/Paris'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 10
+RELATIVE_URLS = True
+
+# Content
+USE_FOLDER_AS_CATEGORY = False
